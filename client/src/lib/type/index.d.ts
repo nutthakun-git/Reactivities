b21a1dd5d87863637c1ebd39024908f669a66809
nutthakun <nutth@mochi.(none)>
2025-04-26 17:49:09 +0700

    End of section 3

---
+++ client/src/lib/type/index.d.ts
@@ -0,0 +1,12 @@
+type Activity = {
+  id: string
+  title: string
+  date: string
+  description: string
+  category: string
+  inCancelled: boolean
+  city: string
+  venue: string
+  latitude: number
+  longitude: number
+}
